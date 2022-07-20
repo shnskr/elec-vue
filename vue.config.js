161203dd24504ce248680500cb356bed94509e99
김달남 <shnskr@ideasolutions.co.kr>
2022-07-20 09:52:27 +0900

    electron 버전 19로 업데이트 및 충돌로 인한 vue.config.js 설정 변경

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        fs: false,
+        path: false
+      }
+    }
+  },
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true
+    }
+  }
 })
